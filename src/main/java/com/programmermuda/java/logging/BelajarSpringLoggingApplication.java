c8df399601df577db51b4ea63ceafff6364b9787
SAR09 <rizkymuu5@gmail.com>
2024-12-12 18:16:00 +0700

    first commit

---
+++ src/main/java/com/programmermuda/java/logging/BelajarSpringLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.programmermuda.java.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programmermuda/java/logging/BelajarSpringLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmermuda.java.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
